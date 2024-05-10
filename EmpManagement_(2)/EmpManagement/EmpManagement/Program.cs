798e99a822932425cfb2db15f42248daa1070dc5
Roman Jurkovič <98027196+Knight20102010@users.noreply.github.com>
2024-05-10 07:01:24 +0200

    Update Program.cs
    adding comment

--- EmpManagement_(2)/EmpManagement/EmpManagement/Program.cs
+++ EmpManagement_(2)/EmpManagement/EmpManagement/Program.cs
@@ -11,7 +11,7 @@ namespace EmpManagement
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form1()); // metóda ktorá spustí sa štandardná slučka správ aplikácie v aktuálnom vlákne.
         }
     }
 }
