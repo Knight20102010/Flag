175eaa79f330146b1c48630e489c58a3f8d20de0
Roman Jurkovič <98027196+Knight20102010@users.noreply.github.com>
2024-05-10 11:54:31 +0200

    Update Program.cs
    add comment

--- EmpManagement_(2)/EmpManagement/EmpManagement/Program.cs
+++ EmpManagement_(2)/EmpManagement/EmpManagement/Program.cs
@@ -6,7 +6,7 @@ namespace EmpManagement
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main()  //Void používate ako návratový typ metódy (alebo lokálnej funkcie), aby ste určili, že metóda nevracia hodnotu.
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
